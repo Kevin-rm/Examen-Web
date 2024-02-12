2293e61fe58e3a1a5d26f3b894ecda9cc8d22247
Shvmster <118555629+Shvmster@users.noreply.github.com>
2024-02-12 16:32:29 +0300

    ceuilleur

---
+++ functions/ceuilleur/ceuilleur.php
@@ -0,0 +1,25 @@
+<?php 
+//ajouter ceuilleur 
+function add_ceuilleur($nom,$genre,$date_de_naissance,$mot_de_passe)
+{
+    $data=
+    [
+        "nom"=$nom,
+        "genre"=$genre,
+        "date_naissance"=$date_de_naissance,
+        "mot_de_passe"=>hash('sha256', $mot_de_passe)
+
+    ]
+    return add(null,'ceuilleur',$ata );
+}
+
+//ajouter depense 
+function add_depense($id_categorie_depense,$montant)
+{
+    $data=
+    [
+        "id_categorie_depense"=$id_categorie_depense,
+        "montant"=$montant
+    ]
+    return add(null,'depense',$ata );
+}
--- functions/login/login.php
+++ functions/login/login.php
@@ -26,18 +26,6 @@ function add_admin($nom,$pseudo,$mot_de_passe)
     return add(null,'admin',$data );
 }
 
-function add_ceuilleur($nom,$genre,$date_de_naissance,$mot_de_passe)
-{
-    $data=
-    [
-        "nom"=$nom,
-        "genre"=$genre,
-        "date_naissance"=$date_de_naissance,
-        "mot_de_passe"=>hash('sha256', $mot_de_passe)
-
-    ]
-    return add(null,'ceuilleur',$ata );
-}
 
 
 
