43e0d6b047b43d97fe2b7e5bd05cb5f12e78120f
Kevin-rm <kevinramarozatovo@gmail.com>
2024-02-12 14:45:55 +0300

    rearrange the conception.sql

--- database/script/conception.sql
+++ database/script/conception.sql
@@ -1,8 +1,40 @@
-create database ceuillette ;
-create table admin(id int primary key , pseudo varchar(30), mot_de_passe varchar(20),nom varchar(50)) engine=innodb;
-create table ceuilleur(id int primary key, nom varchar(50), genre varchar(1),date_naissance date , mot_de_passe varchar(20)) engine=innodb;
-create table the( id int primary key , occupation int , rendement int ) engine=innodb;
-create table parcelle(id int primary key , surface int )  engine=innodb;
-create table relation_variete_the_parcelle unique(id_parcelle int , id_the int ) engine=innodb;
-create table categorie_depense (id int primary key , categorie varchar(20)) engine=innodb;
+DROP   DATABASE gestion_the;
+CREATE DATABASE gestion_the;
+USE             gestion_the;
 
+CREATE TABLE admin (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(50),
+    pseudo VARCHAR(30), 
+    mot_de_passe VARCHAR(100)
+) ENGINE=InnoDB;
+
+CREATE TABLE cueilleur (
+    id             INT PRIMARY KEY AUTO_INCREMENT, 
+    nom            VARCHAR(50),
+    genre          VARCHAR(1),
+    date_naissance DATE,
+    mot_de_passe   VARCHAR(100)
+) ENGINE=InnoDB;
+
+CREATE TABLE variete_the ( 
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    nom        VARCHAR(50),
+    occupation INT, -- En pieds 1pied = 1,8 m²
+    rendement  INT  -- Rendement par pied(kg de feuille par mois)
+) ENGINE=InnoDB;
+
+CREATE TABLE parcelle (
+    id      INT PRIMARY KEY AUTO_INCREMENT, 
+    surface INT
+) ENGINE=InnoDB;
+
+CREATE TABLE rel_variete_the_parcelle (
+    id_variete_the INT REFERENCES variete_the(id),
+    id_parcelle    INT REFERENCES parcelle(id)
+) ENGINE=InnoDB;
+
+CREATE TABLE categorie_depense (
+    id INT PRIMARY KEY, 
+    categorie VARCHAR(20)
+) ENGINE=InnoDB;
