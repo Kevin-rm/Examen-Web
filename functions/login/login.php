93416b3461a2d60e5a8f0bbe217b98bc1c5f7430
Shvmster <118555629+Shvmster@users.noreply.github.com>
2024-02-12 15:30:36 +0300

    new login

--- functions/login/login.php
+++ functions/login/login.php
@@ -1,11 +1,15 @@
 <?php
+include ("../../database/db_connector.php");
+
 function login($nom_table,$email,$mot_de_passe)
 {
     $mot_de_passe = hash('sha1', $mot_de_passe);
-    $requetet="select * from '%s'";
-    $requete=sprintf($requete,$nom_table);
-    $result = mysqli_query(dbconnect(), $requete);
-    while($data=mysqli_fetch_assoc($result))
+    $requete = "SELECT * FROM {$nom_table}";
+    $stmt = get_mysql_connection()->prepare($requete);
+    $stmt->execute();
+ 
+    // Récupération des résultats
+    while ($data = $stmt->fetch(PDO::FETCH_ASSOC))        
     {
         if($email !=$data['email'] || $mot_de_passe !=$data['mot$mot_de_passe'])
         {
