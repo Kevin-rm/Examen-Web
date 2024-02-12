cd79630baf0dacd5bf0dbab480a330c4be1c6550
Shvmster <118555629+Shvmster@users.noreply.github.com>
2024-02-12 15:22:56 +0300

    login

--- functions/login/login.php
+++ functions/login/login.php
@@ -1,6 +1,21 @@
 <?php
-
-/*
- * Ensembles des fonctions sur le login
- */
+function login($nom_table,$email,$mot_de_passe)
+{
+    $mot_de_passe = hash('sha1', $mot_de_passe);
+    $requetet="select * from '%s'";
+    $requete=sprintf($requete,$nom_table);
+    $result = mysqli_query(dbconnect(), $requete);
+    while($data=mysqli_fetch_assoc($result))
+    {
+        if($email !=$data['email'] || $mot_de_passe !=$data['mot$mot_de_passe'])
+        {
+            $erreur='veuillez reessayer a nouveau';
+            header("Location:#?erreur=".$erreur);
+        }
+        else 
+        {
+            header("Location:#");
+        }
+    }
+}
 
