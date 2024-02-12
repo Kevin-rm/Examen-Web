659f382e6207e857bb2a0f00543945caa40d58a5
Shvmster <118555629+Shvmster@users.noreply.github.com>
2024-02-12 14:58:27 +0300

    database

---
+++ database/script/donnees_test.sql
@@ -0,0 +1,25 @@
+
+insert into admin (nom,pseudo , mot_de_passe) values ('','',sha2('',256),);
+insert into admin (nom,pseudo , mot_de_passe) values ('','',sha2('',256),);
+
+
+insert into ceuilleur(nom, genre , date_naissance, mot_de_passe)values('','',,sha2(,256));
+insert into ceuilleur(nom, genre , date_naissance, mot_de_passe)values('','',,sha2(,256));
+insert into ceuilleur(nom, genre , date_naissance, mot_de_passe)values('','',,sha2(,256));
+insert into ceuilleur(nom, genre , date_naissance, mot_de_passe)values('','',,sha2(,256));
+
+
+insert into variete_the(nom,occupation , rendement) values ('',,);
+insert into variete_the(nom,occupation , rendement) values ('',,);
+
+insert into parcelle(surface)values();
+
+insert into rel_variete_the_parcelle(id_parcelle, id_the)values (,);
+
+insert into categorie_depense(categorie) values ('');
+
+
+
+
+
+
