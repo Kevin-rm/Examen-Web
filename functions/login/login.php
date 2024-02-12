e0a5b5d41371b9f3558e221ab82117cd5fd559a7
Shvmster <118555629+Shvmster@users.noreply.github.com>
2024-02-12 15:45:20 +0300

    login

--- functions/login/login.php
+++ functions/login/login.php
@@ -1,25 +1,18 @@
 <?php
 include ("../../database/db_connector.php");
 
-function login($nom_table,$email,$mot_de_passe)
-{
-    $mot_de_passe = hash('sha1', $mot_de_passe);
-    $requete = "SELECT * FROM {$nom_table}";
-    $stmt = get_mysql_connection()->prepare($requete);
-    $stmt->execute();
- 
-    // Récupération des résultats
-    while ($data = $stmt->fetch(PDO::FETCH_ASSOC))        
-    {
-        if($email !=$data['email'] || $mot_de_passe !=$data['mot$mot_de_passe'])
-        {
-            $erreur='veuillez reessayer a nouveau';
-            header("Location:#?erreur=".$erreur);
-        }
-        else 
-        {
-            header("Location:#");
-        }
-    }
-}
+function get_admin_by_id($id)
+{        
+    $sql ="select * from admin where id = :id";
+    
+    $stmt = get_mysql_connection()->prepare($sql);
+    $stmt->execute(array(':id'=>$id));
+    $name="";
 
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) 
+    {
+        $name = $row['nom'];
+    }
+    return $name;
+
+}
