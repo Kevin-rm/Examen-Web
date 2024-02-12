2ed621c5d444270a8294cb82e11a25fc1e7c316a
Kevin-rm <kevinramarozatovo@gmail.com>
2024-02-12 20:58:49 +0300

    restrucured the views

--- pages/insert_cueillete.php
+++ pages/insert_cueillete.php
@@ -1,13 +1,13 @@
 <div class="container-xxl flex-grow-1 container-p-y">
-    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Insertion/</span> Ceuillette</h4>
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Saisie/</span>Cueillete</h4>
 
     <!-- Layout -->
     <div class="row">
         <div class="col-xl">
             <div class="card mb-4">
                 <div class="card-header d-flex justify-content-between align-items-center">
-                    <h5 class="mb-0">Saisie</h5>
-                    <small class="text-muted float-end">Cueilleur</small>
+                    <h5 class="mb-0">Insertion cueillete</h5>
+                    <small class="text-muted float-end">utilisateur</small>
                 </div>
                 <div class="card-body">
                     <form action="#" method="post">
@@ -19,7 +19,7 @@
                             <label for="cueilleur" class="form-label">Cueilleur</label>
                             <select class="form-select" id="cueilleur" aria-label="Default select example"
                                 name="cueilleur">
-                                <option selected>Choix</option>
+                                <option selected>Choix du cueilleur</option>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
                                 <option value="3">Three</option>
--- pages/layout/layout_2.php
+++ pages/layout/layout_2.php
@@ -70,53 +70,7 @@ $content_page = get_page_to_include($_GET['page']);
 <div class="layout-wrapper layout-content-navbar">
     <div class="layout-container">
         <!-- Menu -->
-
-        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-            <div class="app-brand demo">
-                <a href="#" class="app-brand-link">
-                    <span class="app-brand-text demo menu-text fw-bolder ms-2">Gestion de thé</span>
-                </a>
-
-                <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-                    <i class="bx bx-chevron-left bx-sm align-middle"></i>
-                </a>
-            </div>
-
-            <div class="menu-inner-shadow"></div>
-
-            <ul class="menu-inner py-1">
-                <!-- Dashboard -->
-                <li class="menu-item">
-                    <a href="user-resultat.html" class="menu-link">
-                        <i class="menu-icon tf-icons bx bx-home-circle"></i>
-                        <div data-i18n="Analytics">Dashboard</div>
-                    </a>
-                </li>
-
-                <li class="menu-header small text-uppercase">
-                    <span class="menu-header-text">Insertion</span>
-                </li>
-                <li class="menu-item active open">
-                    <a href="javascript:void(0);" class="menu-link menu-toggle">
-                        <i class="menu-icon tf-icons bx bx-dock-top"></i>
-                        <div data-i18n="Account Settings">Saisie</div>
-                    </a>
-                    <ul class="menu-sub">
-                        <li class="menu-item active">
-                            <a href="form-user-ceuillettes.html" class="menu-link">
-                                <div data-i18n="Account">Cueillette</div>
-                            </a>
-                        </li>
-                        <li class="menu-item">
-                            <a href="form-user-depenses.html" class="menu-link">
-                                <div data-i18n="Notifications">Depense</div>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-
-            </ul>
-        </aside>
+        <?php include_once('../menu.php'); ?>
         <!-- / Menu -->
 
         <!-- Layout container -->
