6187e12c1d2e37b45c4f3c387eb40946aab3595d
Shvmster <118555629+Shvmster@users.noreply.github.com>
2024-02-12 16:24:10 +0300

    Update login.php

--- functions/login/login.php
+++ functions/login/login.php
@@ -10,6 +10,34 @@ function get_admin_by_id($id)
 {
     return findWithFilters(null, 'admin', 'id=$id',null);
 }
+function get_cueilleur_by_id($id)
+{
+    return findWithFilters(null, 'cueilleur', 'id=$id',null);
+} 
+
+function add_admin($nom,$pseudo,$mot_de_passe)
+{
+    $data=
+    [
+        "nom"=>$nom,
+        "pseudo"=>$pseudo,
+        "mot_de_passe"=>hash('sha256', $mot_de_passe)
+    ]
+    return add(null,'admin',$data );
+}
+
+function add_ceuilleur($nom,$genre,$date_de_naissance,$mot_de_passe)
+{
+    $data=
+    [
+        "nom"=$nom,
+        "genre"=$genre,
+        "date_naissance"=$date_de_naissance,
+        "mot_de_passe"=>hash('sha256', $mot_de_passe)
+
+    ]
+    return add(null,'ceuilleur',$ata );
+}
 
 
 
