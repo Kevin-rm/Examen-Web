20bfd25c61d160b840c6862377d548135225b9f7
Kevin-rm <kevinramarozatovo@gmail.com>
2024-02-12 17:23:08 +0300

    Update traitement.php

--- functions/login/traitement.php
+++ functions/login/traitement.php
@@ -3,24 +3,29 @@
 require_once 'login.php';
 include_once '../utils.php';
 
-//display_var($_POST);
+session_start();
 
-// Formulaire vide
-//if (empty($_POST['pseudo']) || !empty($_POST['password']))
-  //  header('Location: ../../pages/layout/layout_1.php');
+$login_page = 'pages/layout/layout_1.php';
 
 // Absence du statut(admin ou cueilleur)
-if (empty($_POST['statut'])) {
+if (empty($_POST['statut']) || !in_array($_POST['statut'], ['cueilleur', 'admin'])) {
+    $_SESSION['flash_message'] = 'Oups! Une erreur est survenue';
+    header("Location : $login_page" . '?who=cueilleur');
+}
 
+$statut   = $_POST['statut'];
+
+// Formulaire vide
+if (empty($_POST['pseudo']) || !empty($_POST['password'])) {
+    $_SESSION['flash_message'] = 'Veuiller remplir tout le formulaire';
+    header("Location : $login_page" . "?who=$statut");
 }
 
 $pseudo   = $_POST['pseudo'];
 $password = $_POST['password'];
-$statut   = $_POST['statut'];
 
 if ($statut === 'admin') {
     $admin = get_admin_by_pseudo_and_password($pseudo, $password);
-
 } else {
     $cueilleur = get_cueilleur_by_pseudo_and_password($pseudo, $password);
 }
