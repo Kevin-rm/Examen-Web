88c9de7ae9bc51bf1970b3ef2cb4cd66260f1cb5
Shvmster <118555629+Shvmster@users.noreply.github.com>
2024-02-13 14:56:42 +0300

    Update ceuilleur.php

--- functions/ceuilleur/ceuilleur.php
+++ functions/ceuilleur/ceuilleur.php
@@ -64,6 +64,33 @@ function get_poids_total_cueillette($id_cueilleur, $date_min, $date_max)
     return selectFromSQLRaw(null, $query)[0];
 }
 
+//recup poids par parcel en une date donnee
+function get_poids_ceuilli_parcelle_by_date($id_parcelle ,$date)
+{
+    $query="SELECT sum(poids_ceuilli) as value from the_ceuillette".
+    "WHERE EXTRACT(MONTH FROM date_ceuillette)= EXTRACT(MONTH FROM '$date')".
+    "AND choix_parcelle=$id_parcelle";
+    return selectFromSQLRaw(null, $query)[0];
+
+}
+
+//total par parcel 
+function get_total_rendement_par_parcelle($id)
+{
+    $query="SELECT sum(rendement) as value from the_v_rel_variete_the_parcelle ".
+    "WHERE id_parcelle=$id";
+    return selectFromSQLRaw(null, $query)[0];
+}
+
+//restant poids sur parcel
+function get_reste_poids_par_parcelle($date,$id )
+{
+    $total_poids_parcelle=get_total_rendement_par_parcelle($id)->value;
+    $poids_ceuilli_parcelle=get_poids_ceuilli_parcelle_by_date($id ,$date)->value;
+    $poids_restant=$total_poids_parcelle-$poids_ceuilli_parcelle;
+    return $poids_restant;
+}
+
 function get_poids_total_cueillette_par_mois( $date_min, $date_max)
 {
     $query = "SELECT SUM(poids_cueilli) AS value FROM the_cueillette " .
@@ -91,7 +118,7 @@ function get_poids_restant($date_min , $date_max)
 
     $poids_restant=$poids_total_global-$poids_par_mois;
 }
-
+/
 function cout_revient($id_cueilleur,$date_min,$date_max)
 {
     $query = "SELECT SUM(montant) AS value FROM the_depense ".
@@ -104,14 +131,44 @@ function cout_revient($id_cueilleur,$date_min,$date_max)
     return $cout_revient;
   
 }
-function add_ceuilette($date_ceuillette,$choix_parcelle,$poids_ceuilli)
+
+function get_ceuilleur_by_id($id)
 {
+    return findWithFilters(null,'the_membre',"id=$id","nom");
+}
+function add_ceuillette($date_ceuillette,$choix_parcelle,$poids_ceuilli)
+{
+    $rqt = findWithFilters($connection, "configuration",null ,"bonus");
+    $sql = findWithFilters($connection, "configuration",null ,"mallus");
     $data=
     [
         "date_ceuillette"=>$date_ceuillette,
         "choix_parcelle"=>$choix_parcelle,
-        "poids_ceuilli"=>$poids_ceuilli
+        "poids_ceuilli"=>$poids_ceuilli,
+        "bonus"=>$rqt,
+        "mallus"=>$sql
     ];
     return add(null,'the_ceuillette',$data );
 }
+
+//obtenir la liste des payements
+function get_liste_payement($id_cueilleur,$date_min,$date_max)
+{
+    return findWithFilters(null, 'the_ceuillette', "date_ceuillette >='$date_min' and date_ceuillette<='$date_max' and id_cueilleur=$id_cueilleur","date_ceuillette","poids_ceuilli","bonus","mallus" );
+}
+
+//prendre salaire a inserer dans liste payement
+function get_salaire($poids)
+{
+    $salaire_par_kg=findWithFilters(null, 'the_configuration',null,"salaire_cueilleur")->value;
+    $salaire=$salaire_par_kg*$poids;
+   return $salaire;
+   
+}
+
+
+
+
+
+
 
